2d41503d239d6496e27a70f97263ef98ae52f58f
Diego Feltrin <diego@dimension.it>
2016-09-21 17:25:26 +0200

    aggiunta unit test

---
+++ SDK/ContactHubSdkLibrary.Test/ContactHubSdkLibrary.Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using ContactHubSdkLibrary.SDKclasses;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactHubSdkLibrary.Test
+{
+    [TestFixture]
+    public class CHubNodeTest
+    {
+        [Test]
+        public void CHubNode_Init()
+        {
+            CHubNode node = new CHubNode("c", "xx", "xx");
+ 
+            Assert.That(node,! Is.Null);
+        }
+    }
+}
